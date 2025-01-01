3850b16a7989fe4183c6112b45e3dc8ce20e9aba
Yves Bertot (he) <yves.bertot@inria.fr>
2025-01-01 21:58:41 +0100

    the structure of the common invariant proof is explicit, the easy cases done

--- theories/second_degenerate_position.v
+++ theories/second_degenerate_position.v
@@ -145,3 +145,87 @@ Notation open_cell_side_limit_ok :=
   (@open_cell_side_limit_ok R).
 
  (* End of notation prefix. *)
+
+Definition last_case fop lsto fc' cc lcc lc cls lstc he ev :=
+  let (nos, lno) := update_open_cell_top lsto he ev in
+  Bscan (fop ++ fc' ++ nos) lno lc
+    (closing_cells (point ev) (behead cc) ++ lstc ::cls)
+    (close_cell (point ev) lcc) he (p_x (point ev)).
+
+Lemma last_case_common_invariant
+  bottom top s fop lsto lop cls lstc ev
+  lsthe lstx evs :
+  bottom <| top ->
+  {in bottom :: top :: s &, forall e1 e2, inter_at_ext e1 e2} ->
+  {in s, forall g, inside_box bottom top (left_pt g) &&
+                   inside_box bottom top (right_pt g)} ->
+  lstx = p_x (point ev) ->
+  point ev <<= lsthe ->
+  point ev >>= lsthe ->
+  common_non_gp_invariant bottom top s
+     (Bscan fop lsto lop cls lstc lsthe lstx)
+     (ev :: evs) ->
+  common_invariant bottom top s
+     (step (Bscan fop lsto lop cls lstc lsthe lstx) ev)
+    evs.
+Proof.
+move=> boxwf nocs' inbox_es at_lstx pu pa comng.
+have comi := ngcomm comng.
+rewrite /step /= /same_x at_lstx eqxx pu (negbTE pa) /=.
+case oca_eq : open_cells_decomposition => [[[[[fc' cc] lcc] lc] le] he].
+have oute : out_left_event ev.
+  by apply: (out_events comi); rewrite inE eqxx.
+have := inv1 comi; rewrite /state_open_seq/=.
+move=> -[] clae [] [// | sval] [] adj [] cbtom rfo.
+have pal : (point ev) >>> low lsto.
+  by exact: (same_x_point_above_low_lsto at_lstx comng).
+have noc : {in all_edges (fop ++ lsto :: lop) (ev :: evs) &,
+           no_crossing R}.
+  apply: inter_at_ext_no_crossing.
+  by apply: (sub_in2 (edges_sub comi) nocs').
+set st := last_case fop lsto fc' cc lcc lc cls lstc he ev.
+have inv1' : inv1_seq bottom top evs
+  (state_open_seq (last_case fop lsto fc' cc lcc lc cls lstc he ev)).
+  have := (step_keeps_invariant1 cls lstc (inbox_events (ngcomm comng))
+    oute rfo cbtom adj sval (closed_events comi) clae 
+    (esym (high_lsto_eq comi)) (fun x : p_x (point ev) = lstx => pal) noc
+    (lex_events comi)).
+  rewrite /invariant1 /step /same_x at_lstx eqxx pu (negbTE pa) /=.
+  by rewrite oca_eq.
+have lstx_eq' : lst_x _ _ st = left_limit (lst_open st).
+  rewrite /st/last_case.
+  admit.
+have high_lsto_eq' : high (lst_open st) = lst_high _ _ st.
+  admit.
+have edges_sub' : {subset all_edges (state_open_seq st) evs <=
+  [:: bottom, top & s]}.
+  admit.
+have closed_events' : close_edges_from_events evs.
+  by have := closed_events comi => /= /andP[].
+have out_events' : {in evs, forall e, out_left_event e}.
+  by move=> e ein; apply: (out_events comi); rewrite inE ein orbT.
+have uniq_ec' : {in evs, forall e, uniq (outgoing e)}.
+  by move=> e ein; apply: (uniq_ec comi); rewrite inE ein orbT.
+have inbox_events' : all (inside_box bottom top)
+           [seq point x | x <- evs].
+  by have := inbox_events comi=> /= /andP[].
+have no_dup_edge' : {in [seq high c | c <- state_open_seq st] & evs,
+     forall g e, g \notin (outgoing e)}.
+  admit.
+have lex_events' : sorted (@lexPtEv _) evs.
+  have := lex_events comi; rewrite /= (path_sortedE (@lexPtEv_trans _)).
+  by move=> /andP[]. 
+have sides_ok' : all open_cell_side_limit_ok (state_open_seq st).
+  admit.
+have above_low_lsto' :
+     {in evs, forall e,
+        lexPt (last dummy_pt (left_pts (lst_open st)))
+              (point e)}.
+  admit.
+have stradle' : 
+     evs = [::] \/ {in [seq high c | c <- state_open_seq st], forall g,
+     lexPt (left_pt g) (point (head dummy_event evs)) &&
+     lexePt (point (head dummy_event evs)) (right_pt g)}.
+  admit.
+by constructor.
+Admitted.
