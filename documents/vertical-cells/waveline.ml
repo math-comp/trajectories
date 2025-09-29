022aeb4097e91faf05cd1ec7b7d91e72e3c83315
Yves Bertot <yves.bertot@inria.fr>
2025-09-29 16:58:37 +0200

    work on figures to make them more readable

--- documents/vertical-cells/waveline.ml
+++ documents/vertical-cells/waveline.ml
@@ -1,18 +1,19 @@
-let offset = 5;
+let offset = 5;;
 
 let one_wave x y =
-  [ string_of_int x; string_of_int y; "moveto";
-    string_of_int (x + offset); string_of_int (y + offset);
+  [ string_of_int (x + offset); string_of_int (y + offset);
     string_of_int (x + offset); string_of_int (y + 2 * offset);
     string_of_int x; string_of_int (y + 3 * offset); "curveto";
     string_of_int (x - offset); string_of_int (y + 4 * offset);
     string_of_int (x - offset); string_of_int (y + 5 * offset);
-    string_of_int x; string_of_int (y + 6 * offset); "curveto"]
+    string_of_int x; string_of_int (y + 6 * offset); "curveto\n"];;
     
 
-let rec vertical_waveline x1 y1 length =
-  if length < 30 then
-  [ string_of_int x1; string_of_int y1; "moveto";
-    string_of_int x1; string of_int (y1 + length); "lineto" ]
+let rec vertical_waveline_aux x1 y1 length =
+  if length < 6 * offset then
+  [ string_of_int x1; string_of_int (y1 + length); "lineto\n" ]
   else
-    one_wave x1 y1 @ vertical_waveline x1 (y1 + 30) (length - 30)
+    one_wave x1 y1 @ vertical_waveline_aux x1 (y1 + 30) (length - 30);;
+
+let vertical_wave_line x1 y1 length =
+  print_string (String.concat " " ("" :: vertical_waveline_aux x1 y1 length));;
